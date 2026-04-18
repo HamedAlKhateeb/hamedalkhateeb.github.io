cf076a99dbb4dddf5125caa701925182d2fba219
Your Name <you@example.com>
2026-04-18 10:20:09 +0200

    Move blog to primary domain

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "ar-SA",
-    baseUrl: "HamedAlKhateeb.github.io/quartz",
+    baseUrl: "HamedAlKhateeb.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
