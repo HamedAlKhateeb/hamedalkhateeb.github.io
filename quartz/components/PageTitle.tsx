cc76183cbe0745572782933ee773c0eb874a4ae1
Your Name <you@example.com>
2026-04-18 11:06:00 +0200

    Update site title to link explicitly to root domain

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -9,7 +9,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   return (
     <div class={classNames(displayClass, "page-title-container")}>
       <h1 class="page-title">
-        <a href={baseDir}>{title}</a>
+        <a href="https://hamedalkhateeb.github.io/">{title}</a>
       </h1>
       <p class="page-subtitle">ما صفحت الأقلام، ما طوى الطير، وما يروي العليل</p>
     </div>
