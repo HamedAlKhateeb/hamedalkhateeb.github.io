352f7178830ddd58b9320d1a3a271620010e46bd
Your Name <you@example.com>
2026-04-19 17:11:18 +0200

    Add Google Analytics gtag

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -41,6 +41,13 @@ export default (() => {
 
     return (
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-3QGXK7JG0G"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-3QGXK7JG0G');
+        `}}></script>
         <title>{title}</title>
         <meta charSet="utf-8" />
         {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
