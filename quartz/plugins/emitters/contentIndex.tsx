15b7441bd2c5b4f275bc93125d8d6d473a499839
Your Name <you@example.com>
2026-06-24 16:35:10 +0300

    fix(sitemap): remove unused xhtml namespace from urlset

--- quartz/plugins/emitters/contentIndex.tsx
+++ quartz/plugins/emitters/contentIndex.tsx
@@ -50,7 +50,7 @@ function generateSiteMap(cfg: GlobalConfiguration, idx: ContentIndexMap): string
     .map(([slug, content]) => createURLEntry(simplifySlug(slug), content))
     .join("\n")
   return `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${urls}
 </urlset>`
 }
